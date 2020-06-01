413a2c05b06b15de046955bae7cb11165f2bc8d4
julioponce <julioponce@julioponce.com>
2020-06-01 10:24:20 -0500

    access test

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { render } from 'react-dom'
 import Root from '@/components/Root'
-// comentario test
+
 import '@/assets/scss/reset.scss'
 import '@/assets/scss/typography.scss'
 
